534bff6a95409353467eccc343c338c260e033e8
Meow <nguyendangdu2001@gmail.com>
2022-05-30 15:42:21 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,16 @@
-// import { defineConfig } from "vite";
-// import reactRefresh from "@vitejs/plugin-react-refresh";
-// import { resolve } from "path";
-// import eslint from "@rbnlffl/rollup-plugin-eslint";
-// // import eslintPlugin from "vite-plugin-eslint";
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     reactRefresh(),
-//     eslint({
-//       extensions: ["js", "jsx", "ts", "tsx"],
-//       filterInclude: ["**/*.{js,ts,jsx,tsx}"],
-//       throwOnError: true,
-//     }),
-//   ],
-//   // define: {
-//   //   global: "window",
-//   // },
-//   resolve: {
-//     alias: {
-//       "@pages": resolve(__dirname, "./src/pages"),
-//       "@modules": resolve(__dirname, "./src/modules"),
-//       "@routes": resolve(__dirname, "./src/routes"),
-//       "@app": resolve(__dirname, "./src/app"),
-
-//       "@hooks": resolve(__dirname, "./src/common/hooks"),
-//       "@components": resolve(__dirname, "./src/common/components"),
-//       "@action": resolve(__dirname, "./src/redux/action"),
-//       "@config": resolve(__dirname, "./src/config"),
-//       "@helper": resolve(__dirname, "./src/helper"),
-//       "@assets": resolve(__dirname, "./src/assets"),
-//       "@data": resolve(__dirname, "./src/data"),
-//       "@redux": resolve(__dirname, "./src/redux"),
-//       "@icon": resolve(__dirname, "./src/common/components/icons"),
-//     },
-//   },
-// });
-import reactRefresh from "@vitejs/plugin-react-refresh";
 import eslint from "@rbnlffl/rollup-plugin-eslint";
+import react from "@vitejs/plugin-react";
+
 import { resolve } from "path";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+
 // https://vitejs.dev/config/
 /**
  * @type {import('vite').UserConfig}
  */
 const config = ({ command }) => ({
   plugins: [
-    reactRefresh(),
+    react(),
     eslint({
       extensions: ["js", "jsx", "ts", "tsx"],
       filterInclude: ["**/*.{js,ts,jsx,tsx}"],
@@ -55,9 +21,9 @@ const config = ({ command }) => ({
   build: {
     manifest: true,
   },
-  // define: {
-  //   global: "window",
-  // },
+  define: {
+    "process.env": process.env,
+  },
   resolve: {
     alias: {
       "@pages": resolve(__dirname, "./src/pages"),
@@ -76,6 +42,20 @@ const config = ({ command }) => ({
       "@redux": resolve(__dirname, "./src/redux"),
       "@icon": resolve(__dirname, "./src/common/components/icons"),
       "@defines": resolve(__dirname, "./src/defines"),
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
     },
   },
 });
